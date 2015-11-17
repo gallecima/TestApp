68091757b66f2a7012f85962a2e51683eebc3e00
gallecima <gallecima@gmail.com>
2015-11-17 17:02:06 -0300

    sdfsdf

---
+++ jsAPP/categorias-in.js
@@ -0,0 +1,40 @@
+$(document).ready(function(e) {
+
+		waitingDialog.show('Cargando', {dialogSize: 'sm', progressType: 'info'});
+
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var myLatLng = {lat: position.coords.latitude, lng: position.coords.longitude};
+		
+			var lat = position.coords.latitude;
+			var lng = position.coords.longitude;
+	
+			$.ajax({
+				method: "POST",
+				dataType: "json",
+				jsonp: 'jsoncallback',
+				crossDomain:true,
+				url: 'http://www.pixelio.com.ar/ubiquo/appFunctions/listarOfertas.php',
+				data: { id_categoria: window.localStorage.getItem("id-categoria"), lat: lat, lng: lng}
+			})
+			.done(function( result ) {
+				var html = "";
+				$.each(result, function () {
+					if(this.id>0)
+					{
+						html +=    "<a href='#' class='list-group-item link' data-id='"+this.id+"'><h5>"+this.nombreEmpresa+"<i class='fa fa-caret-right pull-right'></i></h5><p>Texto Descriptivo</p></a>";
+					}
+				});
+	
+				$("#listaOfertas").append(html);
+				
+				
+				waitingDialog.hide();
+			});
+	
+		});			
+
+		
+
+			
+
+});
--- jsAPP/categorias.js
+++ jsAPP/categorias.js
@@ -14,7 +14,7 @@ $(document).ready(function(e) {
 			$.each(result, function () {
 				if(this.id>0)
 				{
-          			html += "<a href='#' class='list-group-item link' data-id='"+this.id+"'><h4 class='list-group-item-heading'><i class='"+this.icono+"'></i>"+this.nombre+"<i class='fa fa-caret-right pull-right'></i></h4></a>";
+          			html += "<a href='#' class='list-group-item link' data-id='"+this.id+"'><h5><i class='"+this.icono+"'></i>"+this.nombre+"<i class='fa fa-caret-right pull-right'></i></h5></a>";
 				}
 			});
 
